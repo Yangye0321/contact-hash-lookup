82d91cb701da6b34b88d2a7d5c05b014b18b2fe8
YY <3090039786@qq.com>
2026-06-21 22:30:33 +0800

    增加插入，查找，删除功能

--- HashContacts.cpp
+++ HashContacts.cpp
@@ -41,7 +41,7 @@ void TraverseHash(Buckets* con)  //遍历哈希表
     }
 }
 
-void CreateHash(Buckets* con,HashNode data[],int n)
+void CreateHash(Buckets* con,HashNode data[],int n)  //创建哈希表
 {
     int h;
     HashNode* p;
@@ -63,6 +63,65 @@ void CreateHash(Buckets* con,HashNode data[],int n)
         p->next=s;
     }
 }
+
+void InsertHash(Buckets* con,HashNode data)  //插入哈希表
+{
+    int h=BKDRHash(&data.name[0]);
+    HashNode* p=con[h].head;
+    int t=1;
+    while(p->next!=NULL)
+    {
+        t++;
+        p=p->next;
+    }
+    HashNode* s=new HashNode;
+    s->name=data.name;
+    s->pn=data.pn;
+    s->st=t;
+    s->next=NULL;
+    p->next=s;
+}
+
+void SearchHash(Buckets* con,string name)  //搜索哈希表
+{
+    int h=BKDRHash(&name[0]);
+    HashNode* p=con[h].head->next;
+    while(p!=NULL)
+    {
+        if(p->name==name)
+        {
+            cout<<p->st<<" "<<p->name<<" "<<p->pn<<" "<<endl;
+            return;
+        }
+        p=p->next;
+    }
+    cout<<"Not found!"<<endl;
+}
+
+void DeleteHash(Buckets* con,string name)  //删除哈希表中的节点
+{
+    int h=BKDRHash(&name[0]);
+    HashNode* p=con[h].head;
+    bool flag=false;
+    while(p->next!=NULL)
+    {
+        if(flag)
+        {
+            p->st--;
+        }
+        if(p->next->name==name)
+        {
+            HashNode* q=p->next;
+            p->next=q->next;
+            delete q;
+            cout<<"Deleted!"<<endl;
+            flag=true;
+            return;
+        }
+        p=p->next;
+    }
+    cout<<"Not found!"<<endl;
+}
 int main()
 {
     Buckets* contacts = new Buckets[Maxsize];
@@ -74,6 +133,11 @@ int main()
     int n=3;
     HashNode data[3]={{"张三","1"},{"李四","2"},{"王五","3"}};
     CreateHash(contacts,data,n);
-    TraverseHash(contacts);
+    //TraverseHash(contacts);
+    SearchHash(contacts,"张三");
+    DeleteHash(contacts,"张三");
+    SearchHash(contacts,"张三");
+    InsertHash(contacts,{"赵六","4"});
+    SearchHash(contacts,"赵六");
     return 0;
 }
