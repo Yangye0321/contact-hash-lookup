ccebe68bf8e4eaaa3ab4bd5d810a1ba361cec534
YY <3090039786@qq.com>
2026-06-21 23:22:35 +0800

    划分出多文件

--- Functions.h
+++ Functions.h
@@ -1,50 +1,28 @@
+#include<iostream>
+#include<fstream>
 #define Maxsize 40
-unsigned int BKDRHash(char* str) //BKDRHash函数 
+using namespace std;
+struct HashNode     //哈希表中的节点
 {
-    unsigned int seed=131;
-    unsigned int hash=0;
-    
-    while(*str)
-    {
-        hash=hash*seed+(*str++);
-    }
-    
-    return hash%Maxsize;
-}
-
-void TraverseHash(Buckets* con)  //遍历哈希表
+    string name;
+    string pn;     //phonenumber
+    string addr;
+    int st;        //searchtime
+    HashNode* next;
+};
+struct Buckets      //存放哈希表的桶
 {
-    HashNode* p;
-    for(int i=0;i<Maxsize;i++)
-    {
-        p=con[i].head->next;
-        while(p!=NULL)
-        {
-            cout<<p->st<<" "<<p->name<<" "<<p->pn<<" "<<endl;
-            p=p->next;
-        }
-    }
-}
-
-void CreateHash(Buckets* con,HashNode data[],int n)
+    HashNode* head;  //头节点
+};
+class Functions
 {
-    int h;
-    HashNode* p;
-    int t=1;
-    for(int i=0;i<n;i++)
-    {
-        h=BKDRHash(&data[i].name[0]);
-        p=con[h].head;
-        while(p->next!=NULL)
-        {
-            t++;
-            p=p->next;
-        }
-        HashNode* s=new HashNode;
-        s->name=data[i].name;
-        s->pn=data[i].pn;
-        s->st=t;
-        s->next=NULL;
-        p->next=s;
-    }
-}
+    public:
+    unsigned int BKDRHash(char* str); //BKDRHash函数
+    void TraverseHash(Buckets* con);  //遍历哈希表
+    void CreateHash(Buckets* con,HashNode data[],int n);  //创建哈希表
+    void InsertHash(Buckets* con,HashNode data);  //插入哈希表
+    void SearchHash(Buckets* con,string name);  //查找哈希表
+    void DeleteHash(Buckets* con,string name);  //删除哈希表中的节点
+    void ReadData(Buckets* con);  //从文件中读取数据
+    void WriteData(Buckets* con);  //将数据写入文件
+};
---
+++ functions.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+#include<fstream>
+#include"Functions.h"
+using namespace std;
+#define Maxsize 40
+unsigned int Functions::BKDRHash(char* str) //BKDRHash函数 
+{
+    unsigned int seed=131;
+    unsigned int hash=0;
+    
+    while(*str)
+    {
+        hash=hash*seed+(*str++);
+    }
+    
+    return hash%Maxsize;
+}
+
+void Functions::TraverseHash(Buckets* con)  //遍历哈希表
+{
+    HashNode* p;
+    for(int i=0;i<Maxsize;i++)
+    {
+        p=con[i].head->next;
+        while(p!=NULL)
+        {
+            cout<<p->st<<" "<<p->name<<" "<<p->pn<<" "<<endl;
+            p=p->next;
+        }
+    }
+}
+
+void Functions::CreateHash(Buckets* con,HashNode data[],int n)  //创建哈希表
+{
+    int h;
+    HashNode* p;
+    int t=1;
+    for(int i=0;i<n;i++)
+    {
+        h=BKDRHash(&data[i].name[0]);
+        p=con[h].head;
+        while(p->next!=NULL)
+        {
+            t++;
+            p=p->next;
+        }
+        HashNode* s=new HashNode;
+        s->name=data[i].name;
+        s->pn=data[i].pn;
+        s->st=t;
+        s->next=NULL;
+        p->next=s;
+    }
+}
+
+void Functions::InsertHash(Buckets* con,HashNode data)  //插入哈希表
+{
+    int h=BKDRHash(&data.name[0]);
+    HashNode* p=con[h].head;
+    while(p->next!=NULL)
+    {
+        p=p->next;
+    }
+    HashNode* s=new HashNode;
+    s->name=data.name;
+    s->pn=data.pn;
+    s->st=p->st+1;
+    s->next=NULL;
+    p->next=s;
+}
+
+void Functions::SearchHash(Buckets* con,string name)  //搜索哈希表
+{
+    int h=BKDRHash(&name[0]);
+    HashNode* p=con[h].head->next;
+    while(p!=NULL)
+    {
+        if(p->name==name)
+        {
+            cout<<p->st<<" "<<p->name<<" "<<p->pn<<" "<<endl;
+            return;
+        }
+        p=p->next;
+    }
+    cout<<"Not found!"<<endl;
+}
+
+void Functions::DeleteHash(Buckets* con,string name)  //删除哈希表中的节点
+{
+    int h=BKDRHash(&name[0]);
+    HashNode* p=con[h].head;
+    bool flag=false;
+    while(p->next!=NULL)
+    {
+        if(flag)
+        {
+            p->st--;
+        }
+        if(p->next->name==name)
+        {
+            HashNode* q=p->next;
+            p->next=q->next;
+            delete q;
+            cout<<"Deleted!"<<endl;
+            flag=true;
+            return;
+        }
+        p=p->next;
+    }
+    cout<<"Not found!"<<endl;
+}
+void Functions::ReadData(Buckets* con)  //从文件中读取数据
+{
+    	fstream file;
+		file.open("contact.txt",ios::in);
+        int n;
+		file>>n;
+		HashNode* data=new HashNode[n];
+		for(int i=0;i<n;i++)
+		{
+			file>>data[i].name>>data[i].pn;
+			data[i].st=0;
+		}
+		file.close();
+        CreateHash(con,data,n);
+}
+
+void Functions::WriteData(Buckets* con)  //将数据写入文件
+{
+    fstream file;
+    file.open("contact.txt",ios::out);
+    int count=0;
+    for(int i=0;i<Maxsize;i++)
+    {
+        HashNode* p=con[i].head->next;
+        while(p!=NULL)
+        {
+            count++;
+            p=p->next;
+        }
+    }
+    file<<count<<endl;
+    for(int i=0;i<Maxsize;i++)
+    {
+        HashNode* p=con[i].head->next;
+        while(p!=NULL)
+        {
+            file<<p->name<<" "<<p->pn<<endl;
+            p=p->next;
+        }
+    }
+    file.close();
+}
